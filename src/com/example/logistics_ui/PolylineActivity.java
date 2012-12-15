c0c198cee7f2e927711450706607b862dff44803
websprit <websprit@126.com>
2012-12-15 17:29:24 +0800

    commit

---
+++ src/com/example/logistics_ui/PolylineActivity.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.logistics_ui;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+/**
+ * @author zhenggang.ji
+ *
+ */
+public class PolylineActivity extends android.support.v4.app.FragmentActivity
+        implements OnSeekBarChangeListener {
+
+
+    private GoogleMap mMap;
+    private Polyline mMutablePolyline;
+    private SeekBar mColorBar;
+    private SeekBar mAlphaBar;
+    private SeekBar mWidthBar;
+    
+    private Geocoder geocoder = null;
+    
+    private List<String> locals;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        geocoder = new Geocoder(this,Locale.CHINA);
+        
+        setContentView(R.layout.activity_polyline);
+
+        locals = getIntent().getStringArrayListExtra("locals");
+        Log.d("DEBUG", "locals---"+locals);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+    	
+    	List<LatLng> ptList = new ArrayList<LatLng>();
+    	
+    	for(String locationName:locals){
+    		if(StringUtils.isEmpty(locationName)){
+    			continue;
+    		}
+    		try {
+				List<Address> addressList = geocoder.getFromLocationName(  
+				        locationName, 1);
+				
+				if (addressList != null && addressList.size() > 0) {
+					
+					
+                    double lat = (addressList.get(0).getLatitude());  
+                    double lng = (addressList.get(0).getLongitude());  
+                    
+                    LatLng pt = new LatLng(lat, lng);
+                    ptList.add(pt);
+  
+                } 
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	
+    	Log.d("DEBUG", "ptList---"+ptList);
+    	
+    	LatLng[] latLngArray = ptList.toArray(new LatLng[ptList.size()]);
+    	
+    	Log.d("DEBUG", "latLngArray---"+latLngArray);
+
+        // A simple polyline with the default options from Melbourne-Adelaide-Perth.
+        mMap.addPolyline((new PolylineOptions())
+                .add(latLngArray).geodesic(true).color(Color.BLUE).width(5));
+        
+        mColorBar.setOnSeekBarChangeListener(this);
+        mAlphaBar.setOnSeekBarChangeListener(this);
+        mWidthBar.setOnSeekBarChangeListener(this);
+
+        // Move the map so that it is centered on the mutable polyline.
+        if(ptList!=null && ptList.size()>0)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ptList.get(ptList.size()-1)));
+        
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SYDNEY));
+        
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // Don't do anything here.
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // Don't do anything here.
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (mMutablePolyline == null) {
+            return;
+        }
+
+        if (seekBar == mColorBar) {
+            mMutablePolyline.setColor(Color.HSVToColor(
+                    Color.alpha(mMutablePolyline.getColor()), new float[] {progress, 1, 1}));
+        } else if (seekBar == mAlphaBar) {
+            float[] prevHSV = new float[3];
+            Color.colorToHSV(mMutablePolyline.getColor(), prevHSV);
+            mMutablePolyline.setColor(Color.HSVToColor(progress, prevHSV));
+        } else if (seekBar == mWidthBar) {
+            mMutablePolyline.setWidth(progress);
+        }
+    }
+}
---
+++ src/com/example/logistics_ui/util/LogisticsInfoUtils.java
@@ -0,0 +1,179 @@
+package com.example.logistics_ui.util;
+
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.logistics_ui.model.LogisticsInfo;
+import com.example.logistics_ui.model.TrackInfo;
+
+import android.util.Log;
+
+public class LogisticsInfoUtils {
+	
+	public static void getWuliuInfo(String id,String com, HttpListener listener){
+		String url = "http://api.ickd.cn/?com="+com+"&nu="+id+"&id=D2BCB3066D4D77EC5567712ECA489FC8&type=json&encode=utf8";
+		Log.d("DEBUG", "url: " + url);
+		new HttpDownloadAsyncTask(listener).execute(url);
+	}
+	
+	
+	/**
+	 * @param str
+	 * @return
+	 */
+	public static JSONArray getLocationJasonArray(String str){
+		JSONArray jsonArray = null;
+		try {
+			jsonArray = new JSONArray(str);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return jsonArray;
+	}
+
+	
+	/**
+	 * 从json解析，获取Book对象
+	 * @param jsonStr
+	 * @return
+	 */
+	public static LogisticsInfo parseWuliuInfo(String jsonStr){
+		
+		JSONObject json = null;
+		Log.d("DEBUG", "jsonStr: " + jsonStr);
+		LogisticsInfo wuliuInfo = new LogisticsInfo();
+		try {
+			json = new JSONObject(jsonStr);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(json!=null) {
+			wuliuInfo.init(json);
+			return wuliuInfo;
+		}else{
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * @param info
+	 * @return
+	 */
+	public static Set<String> getLocations(LogisticsInfo info,
+			Map<String, Map<String, Set<String>>> dizhi) {
+		Set<String> locations = new LinkedHashSet<String>();
+		if(info==null){
+			return locations;
+		}
+		
+		if (info.getTrackInfoList() != null)
+			for (TrackInfo trackInfo : info.getTrackInfoList()) {
+				String context = trackInfo.getContext();
+				if (StringUtils.isNotBlank(context)) {
+					context = StringUtils.substringBeforeLast(context, "正发往");
+					context = StringUtils.substringBeforeLast(context, "分拨中心");
+					String local = "";
+					for (Map.Entry<String, Map<String, Set<String>>> entry : dizhi
+							.entrySet()) {
+						if (context.contains(entry.getKey())
+								|| context.contains(remove(entry.getKey()))) {
+							local = local + entry.getKey();
+							for (Map.Entry<String, Set<String>> subEntry : entry
+									.getValue().entrySet()) {
+								if (StringUtils
+										.equals(subEntry.getKey(), "市辖区")
+										|| StringUtils.equals(
+												subEntry.getKey(), "县")
+										|| StringUtils.equals(
+												subEntry.getKey(),
+												"自治区直辖县级行政区划")) {
+									for (String str : subEntry.getValue()) {
+										if (context.contains(str)
+												|| context
+														.contains(remove(str))) {
+											local = local + str;
+											break;
+										}
+									}
+
+								} else if (context.contains(subEntry.getKey())
+										|| context.contains(remove(subEntry
+												.getKey()))) {
+									local = local + subEntry.getKey();
+									for (String str : subEntry.getValue()) {
+										if (context.contains(str)
+												|| context
+														.contains(remove(str))) {
+											local = local + str;
+											break;
+										}
+									}
+								}
+							}
+							break;
+						} else {
+							for (Map.Entry<String, Set<String>> subEntry : entry
+									.getValue().entrySet()) {
+								if (StringUtils
+										.equals(subEntry.getKey(), "市辖区")
+										|| StringUtils.equals(
+												subEntry.getKey(), "县")
+										|| StringUtils.equals(
+												subEntry.getKey(),
+												"自治区直辖县级行政区划")) {
+									for (String str : subEntry.getValue()) {
+										if (context.contains(str)
+												|| context
+														.contains(remove(str))) {
+											local = local + str;
+											break;
+										}
+									}
+
+								} else if (context.contains(subEntry.getKey())
+										|| context.contains(remove(subEntry
+												.getKey()))) {
+									local = local + subEntry.getKey();
+									for (String str : subEntry.getValue()) {
+										if (context.contains(str)
+												|| context
+														.contains(remove(str))) {
+											local = local + str;
+											break;
+										}
+									}
+								}
+							}
+						}
+					}
+					locations.add(local);
+				}
+			}
+		return locations;
+	}
+	
+	/**
+	 * @param str
+	 * @return
+	 */
+	public static String remove(String str){
+		String temp =new String(str);
+		if(!StringUtils.equals("市辖区", temp)){
+			return temp.replace("市", "").replace("省", "").replace("区", "").replace("县", "").replace("自治区", "").replace("自治州", "");
+		} else {
+			return temp;
+		}
+		
+	}
+
+	
+}
