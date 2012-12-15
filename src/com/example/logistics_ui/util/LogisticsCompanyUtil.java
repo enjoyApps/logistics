5c607317b4b3d473a83dc2135b88d1b422c8152e
websprit <websprit@126.com>
2012-12-15 15:41:35 +0800

    update

--- src/com/example/logistics_ui/util/LogisticsCompanyUtil.java
+++ src/com/example/logistics_ui/util/LogisticsCompanyUtil.java
@@ -3,6 +3,8 @@ package com.example.logistics_ui.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.example.logistics_ui.model.LogisticsCompany;
 
 public class LogisticsCompanyUtil {
@@ -14,6 +16,69 @@ public class LogisticsCompanyUtil {
 		logisticsCompanyList.add(new LogisticsCompany("汇通快运","huitong","10086"));
 		logisticsCompanyList.add(new LogisticsCompany("顺丰速递","shunfeng","10086"));
 		logisticsCompanyList.add(new LogisticsCompany("汇通快运","huitong","10086"));
+	}
+	
+	/**
+	 * @return
+	 */
+	public static List<LogisticsCompany> getAllLogisticsCompany() {
+		return logisticsCompanyList;
+	}
+	
+	/**
+	 * @param name
+	 * @return
+	 */
+	public static String getPhoneNoByNanme(String name){
+		for(LogisticsCompany entry:logisticsCompanyList){
+			if(StringUtils.equalsIgnoreCase(name, entry.getName())){
+				return entry.getPhoneNo();
+				
+			}
+		}
+		return "";
+	}
+	
+
+	/**
+	 * @param pinyin
+	 * @return
+	 */
+	public static String getPhoneNoByPinyin(String pinyin){
+		for(LogisticsCompany entry:logisticsCompanyList){
+			if(StringUtils.equalsIgnoreCase(pinyin, entry.getPinyin())){
+				return entry.getPhoneNo();
+			}
+		}
+		return "";
+	}
+	
+
+	/**
+	 * @param pinyin
+	 * @return
+	 */
+	public static String getNameByPinyin(String pinyin){
+		for(LogisticsCompany entry:logisticsCompanyList){
+			if(StringUtils.equalsIgnoreCase(pinyin, entry.getPinyin())){
+				return entry.getName();
+			}
+		}
+		return "";
+	}
+	
+
+	/**
+	 * @param name
+	 * @return
+	 */
+	public static String getPinyinByName(String name){
+		for(LogisticsCompany entry:logisticsCompanyList){
+			if(StringUtils.equalsIgnoreCase(name, entry.getName())){
+				return entry.getPinyin();
+			}
+		}
+		return "";
 	}
 
 }
