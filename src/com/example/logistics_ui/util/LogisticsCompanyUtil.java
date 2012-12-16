4f2b0e18872f8b0722f32e5f47e3d7f5945dd0e0
websprit <websprit@126.com>
2012-12-16 10:19:03 +0800

    commit

--- src/com/example/logistics_ui/util/LogisticsCompanyUtil.java
+++ src/com/example/logistics_ui/util/LogisticsCompanyUtil.java
@@ -26,6 +26,23 @@ public class LogisticsCompanyUtil {
 		logisticsCompanyList.add(new LogisticsCompany("中通速递","zhongtong","10086"));
 		logisticsCompanyList.add(new LogisticsCompany("宅急送快递","zhaijisong","10086"));
 		logisticsCompanyList.add(new LogisticsCompany("EMS快递","ems","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("圆通快递","yuantong","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("中国邮政平邮","pingyou","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("星晨急便","xingchen","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("中铁快运","zhongtie","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("AAE快递","aae","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("安捷快递","anjie","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("安信达快递","anxinda","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("程光快递","chengguang","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("大田物流","datian","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("德邦物流","debang","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("DHL快递","dhl","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("DPEX快递","dpex","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("D速快递","dsu","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("国际Fedex","fedex","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("Fedex国内","fedexcn","10086"));
+		logisticsCompanyList.add(new LogisticsCompany("原飞航物流","feihang","10086"));
+
 	}
 	
 	/**
